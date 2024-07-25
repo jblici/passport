be39af63c221b788f8a32a5da30f4ec54f000315
Juan Felici <jfelici@agea.com.ar>
2024-07-25 16:37:50 -0300

    http no funciona

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://hap-home.ddns.net:8001/:path*", 
-      },
-    ];
-  },
 };
 
 export default nextConfig;
